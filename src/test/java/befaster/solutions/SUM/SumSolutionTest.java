374b586068025638ee985072664767c119516f6d
sbx_user1051 <sbx_user1051@169.254.80.21>
2019-11-03 14:29:20 +0000

    Sun Nov 03 14:29:20 UTC 2019

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -7,16 +7,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class SumSolutionTest {
-    private SumSolution sum;
 
-    @Before
-    public void setUp() {
-
-        sum = new SumSolution();
-    }
+    private final SumSolution sum = new SumSolution();
 
     @Test
     public void compute_sum() {
-        assertThat(sum.compute(1, 1), equalTo(2));
+        assertThat(sum.compute(1, 3), equalTo(4));
     }
+
 }
+
