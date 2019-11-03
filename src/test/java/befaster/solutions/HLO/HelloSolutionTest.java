869b05d7b42e2c2c79f898bed70ba8d95a1ea6c2
sbx_user1051 <sbx_user1051@169.254.80.21>
2019-11-03 14:36:03 +0000

    Sun Nov 03 14:36:03 UTC 2019

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,15 @@
+package befaster.solutions.HLO;
+
+import org.junit.Test;
+
+
+public class HelloSolutionTest {
+
+    HelloSolution solution = new HelloSolution();
+
+    @Test
+    public void returnsHello() {
+        assertThat();
+    }
+
+}
