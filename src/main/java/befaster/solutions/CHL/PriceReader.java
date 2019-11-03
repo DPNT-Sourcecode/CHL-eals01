ffd35bfd2c01e4d401a19fd130b1dae9395fc5d7
sbx_user1051 <sbx_user1051@169.254.80.21>
2019-11-03 14:46:49 +0000

    Sun Nov 03 14:46:49 UTC 2019

---
+++ src/main/java/befaster/solutions/CHL/PriceReader.java
@@ -0,0 +1,15 @@
+package befaster.solutions.CHL;
+
+public class PriceReader {
+
+    public int readPrice(String sku) {
+        switch (sku) {
+            case "A": return 50;
+            case "B": return 30;
+            case "C": return 20;
+            case "D": return 15;
+            default: throw new IllegalArgumentException();
+        }
+    }
+
+}
--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -1,5 +1,25 @@
-import static org.junit.Assert.*;
+package befaster.solutions.CHL;
+
+import com.shazam.shazamcrest.MatcherAssert;
+import org.junit.Test;
+
+import static com.shazam.shazamcrest.MatcherAssert.assertThat;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
 
 public class CheckliteSolutionTest {
+
+    private final CheckliteSolution solution = new CheckliteSolution();
+
+    @Test
+    public void returnsTotalPriceOfProductsWithoutOffers() {
+        int total = solution.checklite("ABC");
+
+        assertThat(total, sameBeanAs(100));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsIllegalArgumentExceptionForUnknownSku() {
+        solution.checklite("X");
+    }
 
 }
